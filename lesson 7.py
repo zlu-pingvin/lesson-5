f6ded7d2187aebbf820c231fc4bea8abe3768fde
Andrew <zlu.voorhees@gmail.com>
2022-08-21 15:31:09 +0300

    l7

--- lesson 7.py
+++ lesson 7.py
@@ -454,39 +454,77 @@
 #         print(chr(ord(c) - n), end='')
 # print(n)
 
-n = input()
+# n = input()
+#
+# s = n
+# for j in n:
+#     if j in '*,.!@"-':
+#         s = s.replace(j, '')
+#
+# q = sorted(s.split(), key=len)
+# q = q[0]
+# g = [len(q) for i in s.split()]
+#
+#
+# count = 0
+# word_new = ''
+# for d in n:
+#     number = ord(d)
+#     if d == ' ':
+#         count += 1
+#         word_new += chr(number)
+#     elif 65 <= number <= 90:
+#         number += g[count]
+#         if number > 90:
+#             number = number - 26
+#             word_new += chr(number)
+#         else:
+#             word_new += chr(number)
+#     elif 97 <= number <= 122:
+#         number += g[count]
+#         if number > 122:
+#             number = number - 26
+#             word_new += chr(number)
+#         else:
+#             word_new += chr(number)
+#     else:
+#         word_new += chr(number)
+#
+# print(word_new)
 
-s = n
-for j in n:
-    if j in '*,.!@"-':
-        s = s.replace(j, '')
-# g = [len(i) for i in s.split()]
-q = min(n.split())
-g = len(q)
-
-
-count = 0
-word_new = ''
-for d in n:
-    number = ord(d)
-    if d == ' ':
-        count += 1
-        word_new += chr(number)
-    elif 65 <= number <= 90:
-        number += g[count]
-        if number > 90:
-            number = number - 26
-            word_new += chr(number)
-        else:
-            word_new += chr(number)
-    elif 97 <= number <= 122:
-        number += g[count]
-        if number > 122:
-            number = number - 26
-            word_new += chr(number)
-        else:
-            word_new += chr(number)
-    else:
-        word_new += chr(number)
-
-print(word_new)
+# n = input()
+#
+# s = n
+# for j in n:
+#     if j in '*,.!@"-':
+#         s = s.replace(j, '')
+#
+# q = sorted(s.split(), key=len)
+# g = [len(q) for i in s.split()]
+#
+#
+# count = 0
+# word_new = ''
+# for d in n:
+#     number = ord(d)
+#     if d == ' ':
+#         count += 1
+#         word_new += chr(number)
+#     elif 65 <= number <= 90:
+#         number += g[count]
+#         if number > 90:
+#             number = number - 26
+#             word_new += chr(number)
+#         else:
+#             word_new += chr(number)
+#     elif 97 <= number <= 122:
+#         number += g[count]
+#         if number > 122:
+#             number = number - 26
+#             word_new += chr(number)
+#         else:
+#             word_new += chr(number)
+#     else:
+#         word_new += chr(number)
+#
+# print(word_new)
