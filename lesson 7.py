ff051f5c2855e5a9c9679503fb236109c183a478
zlu-pingvin <zlu.voorhees@gmail.com>
2023-04-27 17:28:29 +0300

    l7

--- lesson 7.py
+++ lesson 7.py
@@ -454,39 +454,39 @@
 #         print(chr(ord(c) - n), end='')
 # print(n)
 
-n = input()
-
-s = n
-for j in n:
-    if j in '*,.!@"-':
-        s = s.replace(j, '')
-# g = [len(i) for i in s.split()]
-q = min(n.split())
-g = len(q)
-
-
-count = 0
-word_new = ''
-for d in n:
-    number = ord(d)
-    if d == ' ':
-        count += 1
-        word_new += chr(number)
-    elif 65 <= number <= 90:
-        number += g[count]
-        if number > 90:
-            number = number - 26
-            word_new += chr(number)
-        else:
-            word_new += chr(number)
-    elif 97 <= number <= 122:
-        number += g[count]
-        if number > 122:
-            number = number - 26
-            word_new += chr(number)
-        else:
-            word_new += chr(number)
-    else:
-        word_new += chr(number)
-
-print(word_new)
+# n = input()
+#
+# s = n
+# for j in n:
+#     if j in '*,.!@"-':
+#         s = s.replace(j, '')
+# # g = [len(i) for i in s.split()]
+# q = min(n.split())
+# g = len(q)
+#
+#
+# count = 0
+# word_new = ''
+# for d in n:
+#     number = ord(d)
+#     if d == ' ':
+#         count += 1
+#         word_new += chr(number)
+#     elif 65 <= number <= 90:
+#         number += g[count]
+#         if number > 90:
+#             number = number - 26
+#             word_new += chr(number)
+#         else:
+#             word_new += chr(number)
+#     elif 97 <= number <= 122:
+#         number += g[count]
+#         if number > 122:
+#             number = number - 26
+#             word_new += chr(number)
+#         else:
+#             word_new += chr(number)
+#     else:
+#         word_new += chr(number)
+#
+# print(word_new)
